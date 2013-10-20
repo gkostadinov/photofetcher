7742d37f580f811dcb1ab0c11067a55fcd766cd4
Georgi Kostadinov <grgkostadinov@gmail.com>
2013-10-20 03:21:50 +0300

    Fetch all images from the device, convert them to base64 thumbnails and output the thumbnail, id and real path. Make the plugin call the javascript callback everytime an image is processed.

--- src/android/PhotoFetcher.java
+++ src/android/PhotoFetcher.java
@@ -3,6 +3,8 @@ package com.phonegap.plugins.photofetcher;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 
 import android.content.Context;
 import android.content.ContentResolver;
@@ -11,26 +13,23 @@ import android.database.Cursor;
 import android.database.MergeCursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.media.ThumbnailUtils;
 import android.provider.MediaStore;
 import android.net.Uri;
 import android.util.Log;
+import android.util.Base64;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
 
 public class PhotoFetcher extends CordovaPlugin {
 
     private static final String TAG = "PhotoFetcher";
-    private static final int MAXTHUMBSIZE = 256;
+    private static final int MAXTHUMBSIZE = 128;
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.phonegap.api.Plugin#execute(java.lang.String,
-     * org.json.JSONArray, java.lang.String)
-     */
     @Override
     public boolean execute(String action, final JSONArray args, final CallbackContext callbackContext) {
         Log.d(TAG, "Plugin Called");
@@ -42,16 +41,15 @@ public class PhotoFetcher extends CordovaPlugin {
                         JSONObject result = new JSONObject();
                         JSONArray photos = new JSONArray();
                         JSONObject photo = new JSONObject();
+                        result.put("photos", photos);
 
-                        String[] projection = { MediaStore.Images.Thumbnails._ID };
-
-                        Cursor cursor_external = getContentResolver().query(MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI,
-                                projection,
+                        Cursor cursor_external = getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                                null,
                                 null,
                                 null,
                                 null);
-                        Cursor cursor_internal = getContentResolver().query(MediaStore.Images.Thumbnails.INTERNAL_CONTENT_URI,
-                                projection,
+                        Cursor cursor_internal = getContentResolver().query(MediaStore.Images.Media.INTERNAL_CONTENT_URI,
+                                null,
                                 null,
                                 null,
                                 null);
@@ -63,53 +61,66 @@ public class PhotoFetcher extends CordovaPlugin {
                             cursor_external,
                             cursor_internal
                         });
-                        int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Thumbnails._ID);
-                        int size = cursor.getCount();
 
-                        if (size != 0) {
-                            int imageID = 0;
-                            Bitmap bitmap = null;
-                            Bitmap newBitmap = null;
-                            Uri uri = null;
+                        int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID);
+                        int dataColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                        int widthColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.WIDTH);
+                        int heightColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.HEIGHT);
 
-                            for (int i = 0; i < size; i++) {
-                                cursor.moveToPosition(i);
-                                imageID = cursor.getInt(columnIndex);
+                        if (cursor.moveToFirst()) {
+                            do {
+                                int imageId = cursor.getInt(columnIndex);
+                                String path = cursor.getString(dataColumn);
+                                int width = cursor.getInt(widthColumn);
+                                int height = cursor.getInt(heightColumn);
 
-                                try {
-                                    uri = Uri.withAppendedPath(MediaStore.Images.Thumbnails.INTERNAL_CONTENT_URI, "" + imageID);
-                                    bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
-                                }
-                                catch (Exception ex) {
-                                    uri = Uri.withAppendedPath(MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI, "" + imageID);
-                                    bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
-                                }
+                                if (path != null && width > 200 && height > 200) {
+                                    Log.e(TAG, "Data with image ID " + imageId + ": " + path + ", width: " + width + ", height: " + height);
+                                    double ratio = width/(double)height;
+                                    int newWidth = MAXTHUMBSIZE;
+                                    int newHeight = (int)(newWidth/ratio);
 
-                                if (bitmap != null) {
-                                    newBitmap = ThumbnailUtils.extractThumbnail(
-                                        bitmap, MAXTHUMBSIZE, MAXTHUMBSIZE
-                                    );
-                                    
-                                    bitmap.recycle();
-                                    if (newBitmap != null) {
-                                        photo.put("id", imageID);
-                                        photo.put("uri", uri);
-                                        photo.put("bitmap", newBitmap);
+                                    Bitmap bitmap = ThumbnailUtils.extractThumbnail(
+                                                        BitmapFactory.decodeFile(path),
+                                                        newWidth,
+                                                        newHeight);
+
+                                    if (bitmap != null) {
+                                        ByteArrayOutputStream baos = new ByteArrayOutputStream();  
+                                        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                                        byte[] byteArrayImage = baos.toByteArray();
+                                        String encodedImage = Base64.encodeToString(byteArrayImage, Base64.DEFAULT);
+
+                                        photo.put("id", imageId);
+                                        photo.put("path", path);
+                                        photo.put("data", encodedImage);
 
                                         photos.put(photo);
 
+                                        result.remove("photos");
+                                        result.put("photos", photos);
+
+                                        PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, result);
+                                        pluginResult.setKeepCallback(true);
+                                        callbackContext.sendPluginResult(pluginResult);
+
                                         photo = new JSONObject();
-                                    }                        
+                                    }
                                 }
                             }
+                            while (cursor.moveToNext());
 
+                            result.remove("photos");
+                            result.put("done", true);
                             result.put("photos", photos);
                         } else {
                             result.put("error", "No photos found!");
                         }
                         cursor.close();
 
-                        callbackContext.success(result);
+                        PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, result);
+                        pluginResult.setKeepCallback(false);
+                        callbackContext.sendPluginResult(pluginResult);
                     }
                     catch (JSONException ex) {
                         Log.e(TAG, ex.getMessage());
